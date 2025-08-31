0d30186771f0aca54afd0d61a7e20c8cc4a4a110
Yousef <yza@andrew.cmu.edu>
2025-08-31 18:54:37 +0300

    Fix typing issues in fibonacci function

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,19 @@
+// src/fib.ts
 // util function that computes the fibonacci numbers
-module.exports = function fibonacci(n) {
+
+/**
+ * Computes the n-th Fibonacci number.
+ * @param n - The index of the Fibonacci sequence (must be a non-negative integer)
+ * @returns The n-th Fibonacci number, or -1 if n is negative
+ */
+export function fibonacci(n: number): number {
   if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   }
 
   return fibonacci(n - 1) + fibonacci(n - 2);
-};
+}
